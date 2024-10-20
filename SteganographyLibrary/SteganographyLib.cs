4d5b0e3933e99338d85b7a8d01ee049170c33c1d
Michael Simm <179273005+mickyj0101@users.noreply.github.com>
2024-10-20 17:37:55 +0100

    More development on the steganography library

--- SteganographyLibrary/SteganographyLib.cs
+++ SteganographyLibrary/SteganographyLib.cs
@@ -20,6 +20,14 @@
                 }
                 this.val = val;
             }
+            public SixBit(uint val)
+            {
+                if (val > 63)
+                {
+                    throw new ArgumentOutOfRangeException("'val' parameter must be between 0 and 63");
+                }
+                this.val = (int)val;
+            }
             //Set method
             public void setVal(int newVal)
             {
@@ -65,6 +73,35 @@
                 //Set the pixel to a pixel with the new r, g and b values.
                 map.SetPixel(x, y, new SKColor(r, g, b, pix.Alpha));
             }
+            public void writeToPixel(SKBitmap map, int[] coords)
+            {
+                if (coords.Length != 2)
+                {
+                    throw new ArgumentException("'coords' argument must have length = 2");
+                }
+                writeToPixel(map, coords[0], coords[1]);
+            }
+        }     
+        //Extension method to write an array of SixBits to an image, starting at certain co-ordinates.
+        //Returns the co-ordinates of the pixel after the last pixel with data writen. For example: if you write 10 SixBits of data with startCoords[0, 0]
+        //It will return [10, 0] as this is the first pixel without data written.
+        static int[] writeToImage(this SixBit[] data, SKBitmap map, int[] startCoords)
+        {
+            int[] currentCoords = new int[startCoords.Length];
+            Array.Copy(startCoords, currentCoords, startCoords.Length);
+            foreach (SixBit pix in data)
+            {
+                pix.writeToPixel(map, startCoords);
+                try
+                {
+                    currentCoords.nextPixel(map.Width, map.Height);
+                }
+                catch (ArgumentOutOfRangeException e)
+                {
+                    throw new ArgumentOutOfRangeException("Data is too large to fit in the image");
+                }
+            }
+            return currentCoords;
         }
         //Gets a byte array as SixBits.
         static SixBit[] toSixBits(this byte[] data)
@@ -111,12 +148,13 @@
         }
         //Extension method to get an int as an array of sixbits. If the 'numSixBits' parameter is 0 or blank, then it will return the minimum number of sixbits.
         //Otherwise, it will return that many sixbits. NOTE - it will not throw an error if the number specified is fewer than would be required to fit the data.
-        static SixBit[] toSixBits(this int value, int numSixBits = 0)
+        //It will get the final 'numSixBits' SixBits of the integer.
+        static SixBit[] toSixBits(this uint value, int numSixBits = 0)
         {
             if (numSixBits < 0) throw new ArgumentOutOfRangeException("'numSixBits' argument must be 0 or greater");
             if (numSixBits == 0)
             {
-                int temp = value;
+                uint temp = value;
                 while (temp > 0)
                 {
                     numSixBits++;
@@ -124,12 +162,55 @@
                 }
             }
             SixBit[] result = new SixBit[numSixBits];
-            for (int i = 0; i < numSixBits; i+= 6)
+            for (int i = 0; i < numSixBits; i += 1)
             {
-
+                int pushLeft = 6 * i;
+                if (pushLeft > 30)
+                {
+                    result[numSixBits - (i + 1)] = new SixBit();
+                    continue;
+                }
+                if (pushLeft == 30)
+                {
+                    result[numSixBits - (i + 1)] = new SixBit((value & ((uint)3 << 30)) >> 30);
+                    continue;
+                }
+                result[numSixBits - (i + 1)] = new SixBit((value & ((uint)63 << pushLeft)) >> pushLeft);
             }
+            return result;
 
-
+        }
+        //Convert the pixel number (indexed from 0) to a set of co-ords
+        static int[] pixelNumtoCoords(int width, int height, int num)
+        {
+            if (num >= width * height)
+            {
+                throw new ArgumentOutOfRangeException("'num' parameter must be less than width * height");
+            }
+            int rows = num / width;
+            int cols = num % width;
+            return [cols, rows];
+        }
+        //Extension method - go from a set of coords to the next pixel without converting pixel num to coords (as that other method has division and modulus which are slow)
+        static int[] nextPixel(this int[] currentPixel, int width, int height)
+        {
+            if (currentPixel.Length != 2)
+            {
+                throw new ArgumentException("'currentPixel' argument must have length = 2");
+            }
+            int x = currentPixel[0]++;
+            int y = currentPixel[1];
+            if (x >= width)
+            {
+                y = currentPixel[1]++;
+                currentPixel[0] = 0;
+                x = 0;
+            }
+            if (y >= height)
+            {
+                throw new ArgumentOutOfRangeException("This would go beyond the width and height specified.");
+            }
+            return [x, y];
         }
         //Encode the file at 'filepath' into the image 'img'
         public static SKBitmap encode(SKBitmap img, string filePath)
@@ -151,8 +232,9 @@
             //After this, we need to write the actual binary data within the file. A similar method will be employed.
             //Get the binary data as a byte array, and convert to 6-bit chunks in the same way.
             //Any appended bits are, once again, logged.
-            //Then use 10 pixels to store the number of pixels holding data
-            //This allows for 60 bits, a great deal of redundancy.
+            //Then use 6 pixels to store the number of pixels holding data
+            //This allows for 36 bits (though only 32 will be used), a great deal of redundancy as the 32-bit integer limit is over 517 times the number of pixels
+            //in a 4k display.
             //After this, write the binary data.
             //
             //Reading:
@@ -177,11 +259,31 @@
             {
                 throw new FileNotFoundException("filePath argument does not refer to an existing file.");
             }
+            //Get the filename as a string and byte array, then get the data from the file as a byte array. Though you should typically never read a whole file into
+            //memory, in this case it should be fine as the image itself takes up much more RAM than the file being written to it.
+            //Essentially, this form of steganography only really works with relatively small files.
             string fileName = Path.GetFileName(filePath);
             byte[] nameData = Encoding.UTF8.GetBytes(fileName);
             byte[] fileData = File.ReadAllBytes(filePath);
+            //Get the filename data as an array of SixBits.
             SixBit[] fileNameAsSixbits = nameData.toSixBits();
-            
+            uint nameDataLength = (uint)nameData.Length;
+            //Encode the length of the filename into two sixbits.
+            SixBit[] firstTwo = nameDataLength.toSixBits(2);
+            //Store the width and height of the image as separate variables, just so that it is easier to use.
+            int width = img.Width;
+            int height = img.Height;
+            //Set the starting co-ordinates to [0, 0] or the top-left pixel.
+            int[] coords = [0, 0];
+            //Write the first two SixBits to the image.
+            coords = firstTwo.writeToImage(img, coords);
+            //Write the filename to the image.
+            coords = fileNameAsSixbits.writeToImage(img, coords);
+            SixBit[] fileDataAsSixbits = fileData.toSixBits();
+            uint numPixels = (uint)fileDataAsSixbits.Length;
+            SixBit[] numPixelsSixbits = numPixels.toSixBits(6);
+            coords = numPixelsSixbits.writeToImage(img, coords);
+            coords = fileDataAsSixbits.writeToImage(img, coords);
         }
     }
 }
